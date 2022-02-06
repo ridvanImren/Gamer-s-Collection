ef99a2e2b1d6beb7c8fff98aa5e31ed62e5005e3
ridvanImren <imrenridvan@gmail.com>
2022-02-06 17:43:00 +0300

    Combined framework is now used for fetching data from the API.

--- Gamer's Collection/ViewModels/AddGameViewModel.swift
+++ Gamer's Collection/ViewModels/AddGameViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Combine
 
 @MainActor class AddGameViewModel: ObservableObject {
     @Published var games = Games(data: [Game]())
@@ -19,24 +20,42 @@ import Foundation
         let urlString = "https://api.rawg.io/api/games?key=d8986e4c3c034008a6c7b9782bed45a6&search=\(name)&search_exact=true&platforms=4&stores=1"
         
         
-        guard let url = URL(string: urlString) else {
-            print("URL cannot be created from \(urlString)")
-            return
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .background))
+            .receive(on: DispatchQueue.main)
+            .tryMap( handleOutput )
+            .decode(type: Output.self, decoder: JSONDecoder())
+            .sink { (completion) in
+                
+                // Handling completion
+                switch completion {
+                case .failure(let error):
+                    print("There was an error. \(error)")
+                case .finished:
+                    print("Finished Completion")
+                }
+                
+            } receiveValue: { [weak self] (returnedGames) in
+                self?.games.data = returnedGames.results
+            }
+        
+        
+        func handleOutput(output: Publishers.SubscribeOn<URLSession.DataTaskPublisher, DispatchQueue>.Output) throws -> Data {
+            
+            guard
+                let response = output.response as? HTTPURLResponse,
+                response.statusCode >= 200 && response.statusCode < 300 else {
+                    throw URLError(.badServerResponse)
+                }
+            return output.data
+            
         }
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let output = try JSONDecoder().decode(Output.self, from: data)
-            games.data = output.results
-            
-            
-            
-            
-        } catch {
-            print("N/A")
-        }
+
     }
     
     
-
+    
     
 }
